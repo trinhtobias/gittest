40053cb6e8013482fa340d92671408523a031212
trinhtobias <tritobias@gmail.com>
2018-10-04 12:55:24 +0200

    geändert auf deutsche Version

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("helloWorld");
+    public static void main(String[] args){
+        System.out.println("Hallo Welt!");
     }
 }
